b2bd14c0bfa06f1e0301c7b8f4696ffddc3a181e
Tinnnooo <tinnofabregas01@gmail.com>
2025-12-12 06:52:54 +0000

    feat: add searchable functionality to event filter field

--- src/Pages/Concerns/HasListFilters.php
+++ src/Pages/Concerns/HasListFilters.php
@@ -274,6 +274,7 @@ trait HasListFilters
             ->label(__('filament-activity-log::activities.filters.event'))
             ->visible(fn (callable $get) => $get('subject_type'))
             ->native(false)
+            ->searchable()
             ->options(function (callable $get) {
                 $activityModel = config('activitylog.activity_model') ?? Activity::class;
 
